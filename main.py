5437ba74b6280f667132ab69a25035df3367d3d6
Fer6464 <ariasfernando966@gmail.com>
2026-03-05 06:16:34 +0000

    Corrección errores ortograficos de main.py

--- main.py
+++ main.py
@@ -3,21 +3,21 @@ from info_proyecto import descripcion_proyecto
 
 
 def mostrar_menu():
-    print("\nOpcion 1: Mostrar miembros")
-    print("Opcion 2: Mostrar descripcion")
-    print("Opcion 3: Cerrar programa")
+    print("\nOpción 1: Mostrar miembros")
+    print("Opción 2: Mostrar descripción")
+    print("Opción 3: Cerrar programa")
 
 if __name__ == "__main__":
     opcion = 0
-    print(">>>AVANCE 2 PROYECTO SISTEMAS OPERATIVOS GRUPO 1<<<")
+    print(">>> AVANCE 2 PROYECTO SISTEMAS OPERATIVOS GRUPO 1 <<<")
 
     while opcion != 3:
         mostrar_menu()
 
         try: 
-            opcion = int(input("> Escoja una opcion: "))
+            opcion = int(input("> Escoja una opción: "))
         except(ValueError):
-            print("\nEscriba solo un numero!")
+            print("\nEscriba solo un número!")
             continue
 
         print()
@@ -27,7 +27,7 @@ if __name__ == "__main__":
             case 2:
                 descripcion_proyecto()
             case 3:
-                print("Programa finalizado con exito.")
+                print("Programa finalizado con éxito.")
                 break
             case _:
                 print("Entrada incorrecta!")
