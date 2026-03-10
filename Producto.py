21217befa3a6193f2687fc4fa8d5ea8203111a11
DanVill2603 <dsebasgomez06@gmail.com>
2026-03-09 23:30:34 -0500

    Avance4 (#6)
    * Descripcion del Proyecto
    
    * Implemente las clases (constructor, métodos y atributos) de la aplicación.

---
+++ Producto.py
@@ -0,0 +1,10 @@
+class Producto:
+    def __init__(self, nombre_producto, precio_base, descripcion):
+        self.nombre_producto = nombre_producto
+        self.precio_base = precio_base
+        self.descripcion = descripcion
+
+    def mostrar_info(self):
+        print(f"Producto: {self.nombre_producto}")
+        print(f"Precio base: {self.precio_base}")
+        print(f"Descripción: {self.descripcion}")
